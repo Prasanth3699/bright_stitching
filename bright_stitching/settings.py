194559f7de44ef11d7231910ef5f6f610f2cb2a0
Prasanth3699 <97381509+Prasanth3699@users.noreply.github.com>
2022-08-28 18:12:06 +0530

    Update settings.py

--- bright_stitching/settings.py
+++ bright_stitching/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'bright_stitching.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [TEMPLATE_DIR],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
