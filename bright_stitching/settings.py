3ea8dd74d1535dbe710ebbb7613f15b9238b20de
Prasanth3699 <97381509+Prasanth3699@users.noreply.github.com>
2022-08-28 18:25:45 +0530

    Update settings.py

--- bright_stitching/settings.py
+++ bright_stitching/settings.py
@@ -134,6 +134,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
