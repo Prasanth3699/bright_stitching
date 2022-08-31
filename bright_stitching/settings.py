d0312bf9ce210c2e8e4e5d6491c2ffdd49b1a8f1
prasanth <prasanthranger09@gmail.com>
2022-08-31 20:47:53 +0530

    image setting changes

--- bright_stitching/settings.py
+++ bright_stitching/settings.py
@@ -12,6 +12,15 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
+
+
+
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 BASE_DIR2 = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -133,6 +142,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# cloudinary config
+cloudinary.config( 
+  cloud_name = "prasanth346", 
+  api_key = "524246131792759", 
+  api_secret = "Xe0YqMWt7X1o-TRZdxtfsfgMM7k",
+  secure = True
+)
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
